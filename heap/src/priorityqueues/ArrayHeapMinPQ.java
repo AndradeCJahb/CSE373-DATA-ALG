a1f364fede70e6539ed25268ceb42433ea13919a
Christopher Andrade <andradec@uw.edu>
2023-05-02 11:42:56 -0700

    finish

--- heap/src/priorityqueues/ArrayHeapMinPQ.java
+++ heap/src/priorityqueues/ArrayHeapMinPQ.java
@@ -2,6 +2,8 @@ package priorityqueues;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 /**
  * @see ExtrinsicMinPQ
@@ -9,13 +11,11 @@ import java.util.List;
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     // IMPORTANT: Do not rename these fields or change their visibility.
     // We access these during grading to test your code.
-    static final int START_INDEX = 1;
+    static final int START_INDEX = 0;
     List<PriorityNode<T>> items;
-    // TODO: add fields as necessary
 
     public ArrayHeapMinPQ() {
         items = new ArrayList<>();
-        // TODO: add code as necessary
     }
 
     // Here's a method stub that may be useful. Feel free to change or remove it, if you wish.
@@ -24,43 +24,105 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
      * A helper method for swapping the items at two indices of the array heap.
      */
     private void swap(int a, int b) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        PriorityNode<T> toSwap = items.get(a);
+        items.set(a, items.get(b));
+        items.set(b, toSwap);
     }
 
     @Override
     public void add(T item, double priority) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (contains(item)) {
+            throw new IllegalArgumentException();
+        }
+
+        PriorityNode<T> curr = new PriorityNode<>(item, priority);
+        items.add(curr);
+
+        if (items.size() > 1) {
+            int i = items.size() - 1;
+            while (items.get(i).getPriority() < items.get((i - 1) / 2).getPriority()) {
+                if (i < 1) {
+                    break;
+                }
+                swap(i, (i - 1) / 2);
+                i = (i - 1) / 2;
+            }
+        }
     }
 
     @Override
     public boolean contains(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        for (PriorityNode<T> currItem : items) {
+            if (Objects.equals(currItem.getItem(), item)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public T peekMin() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (items.size() < 1) {
+            throw new NoSuchElementException();
+        }
+        return items.get(0).getItem();
     }
 
     @Override
     public T removeMin() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (items.size() < 1) {
+            throw new NoSuchElementException();
+        }
+
+        if (items.size() == 1) {
+            return items.remove(0).getItem();
+        }
+        swap(0, items.size() - 1);
+        T ret = items.remove(items.size() - 1).getItem();
+        removeMin(0);
+        return ret;
+    }
+
+    private void removeMin(int i) {
+        if (i * 2 + 1 >= items.size()) {
+            return;
+        }
+
+
+        if (i * 2 + 2 < items.size()) {
+            if (items.get(i * 2 + 1).getPriority() > items.get(i).getPriority() &&
+                items.get(i * 2 + 2).getPriority() > items.get(i).getPriority()) {
+                return;
+            }
+            if (items.get(i * 2 + 1).getPriority() > items.get(i * 2 + 2).getPriority() &&
+                items.get(i * 2 + 2).getPriority() < items.get(i).getPriority()) {
+                swap(i * 2 + 2, i);
+                removeMin(i * 2 + 2);
+            } else if (items.get(i * 2 + 1).getPriority() < items.get(i * 2 + 2).getPriority() &&
+                items.get(i * 2 + 1).getPriority() < items.get(i).getPriority()) {
+                swap(i * 2 + 1, i);
+                removeMin(i * 2 + 1);
+            }
+        } else if (items.get(i * 2 + 1).getPriority() < items.get(i).getPriority()) {
+            swap(i * 2 + 1, i);
+            removeMin(i * 2 + 1);
+        }
     }
 
     @Override
     public void changePriority(T item, double priority) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (!contains(item)) {
+            throw new NoSuchElementException();
+        }
+        for (PriorityNode<T> changeItem : items) {
+            if (Objects.equals(changeItem.getItem(), item)) {
+                changeItem.setPriority(priority);
+            }
+        }
     }
 
     @Override
     public int size() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return items.size();
     }
 }
